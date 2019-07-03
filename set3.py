2b3b9cb8b1faaabc82a2f6a7c1cc174344e5c31d
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-03 10:40:27 +0530

    Update set3.py

--- set3.py
+++ set3.py
@@ -1,1 +1,7 @@
-
+character=input()
+if(character=='A' or character=='a'or character=='E' or character=='e' or character=='I' or character=='i' or character=='O' or character=='o'
+   character=='U' or character=='u'):
+  print("Vowel")
+  else:
+    print("Constant")
+  
