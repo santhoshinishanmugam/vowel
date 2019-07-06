a60217648d5a763b2eb6aa97c67c1d148eabe8aa
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-06 11:58:16 +0530

    Update seth1.py

--- seth1.py
+++ seth1.py
@@ -1,1 +1,17 @@
-
+from collections import Counter
+h = input()
+v = [i for in input().split()]
+t = Counter(v)
+r =[]
+for i, j in t.items():
+  if j > 1:
+    r.append(i)
+if r:
+  r.sort()
+  print(' '.join(r))
+else:
+  print('unique')
+  
+             
+    
+   
